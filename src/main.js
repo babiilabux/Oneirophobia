ae06e6dfb5ae18762437223b8f5ab63e2106ab70
babiilabux <babiilabux1@gmail.com>
2025-03-25 11:04:01 +0100

    Ajout du son

--- src/main.js
+++ src/main.js
@@ -221,6 +221,32 @@ function initGame() {
 
 initGame();
 
+// lancement du son
+(async () => {
+  const audioEngine = await BABYLON.CreateAudioEngineAsync();
+
+  // Create sounds here, but don't call `play()` on them, yet ...
+
+  // Wait until audio engine is ready to play sounds.
+  await audioEngine.unlock();
+
+  // Start sound playback ...
+})();
+
+const audioEngine = await BABYLON.CreateAudioEngineAsync();
+
+const ambient = await BABYLON.CreateSoundAsync("ambient",
+  "/public/sounds/ambiance_intro.mp3"
+);
+
+// Set the sound to loop
+ambient.loop = true;
+
+// Wait until audio engine is ready to play sounds.
+await audioEngine.unlock();
+
+// Start playing the sound
+ambient.play();
 
 
 // function createMenu() {
